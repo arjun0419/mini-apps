3b918da4d9f5046cfc15cfdcc301f341bac5799a
Arjun Chemparathy <arjunchemparathy@gmail.com>
2018-02-12 12:01:53 -0800

    challenge1: build MVP view, on click change place value to x or o depending on player

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,32 @@
 
+//On page load, let player X start the game
+//On page load, instantiate a new board
+//on click of a button, change button value to X
 
+function Board() {
+  return {
+    row1: [null, null, null],
+    row2: [null, null, null],
+    row3: [null, null, null],
+    counter: 0
+  }
+};
+
+var newBoardCreator = function() {
+  var newBoard = new Board();
+  return newBoard;
+};
+
+var board = newBoardCreator();
+
+var someFunction = function(event){
+  if(event.target.innerText === '-') {
+    if (board.counter % 2 === 0) {
+      event.target.innerText = "X";
+      board.counter++;
+    } else {
+      event.target.innerText = "O";
+      board.counter++;
+    }
+  }
+}
