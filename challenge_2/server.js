bfe7ef3a99cbddce3b9b985e92ebd5341a7feeea
Arjun Chemparathy <arjunchemparathy@gmail.com>
2018-02-13 12:20:33 -0800

    add express, nodemon. run express app. render static client

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+var express = require('express')
+var app = express();
+
+app.use(express.static('client'));
+ 
+app.get('/', function (req, res) {
+  res.send();
+})
+
+app.get('/convert', function (req, res) {
+  console.log("request to /convert was received");
+  res.send();
+})
+
+app.listen(3000)
 
 
---
+++ challenge_2/todo.js
@@ -0,0 +1,16 @@
+//front End:
+  //index.html should have an input text field and a submit button
+  //it should also have a dom element that shows the csv version of the json afte getting the response data from the server
+
+//backend:
+  //create a server
+    //install express
+    //add the logic for the routes
+    //get request
+      //should render index.html
+    //post request with json
+      //should send the csv data
+  //logic for conversion of json to csv
+
+
+
