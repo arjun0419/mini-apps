71a6175bc42f5c49ba7a7fa75cfe47f80d366a49
Arjun Chemparathy <arjunchemparathy@gmail.com>
2018-02-13 18:00:21 -0800

    bare minimum requirements met

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,30 @@
 
 
+var handleClick = function(event) {
+  $( ".request" ).empty();
+  $( ".response" ).empty();
+  var data = event.path[1].children[0].value;
+  $( ".request" ).append(`<div>${data}</div>`);
+  $(".submitted").css('display', 'block');
+  postData(data);
+};
+
+var postData = function(data) {
+  // console.log(data);
+  $.ajax({
+    method: "POST",
+    contentType: "application/json",
+    url: "http://localhost:3000/convert",
+    data: data
+    })
+    .done(function( msg ) {
+      $( ".response" ).append(`<div>${msg}</div>`);
+      $(".received").css('display', 'block');
+      console.log( "Data received: " + msg );
+  });
+}
+
+
+//on click of submit button
+ //make an ajax get rquest with the data captured
+
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,17 +1,90 @@
 var express = require('express')
 var app = express();
 
+//load static html page and app.js from client folder
 app.use(express.static('client'));
  
+//shows index page 
 app.get('/', function (req, res) {
   res.send();
 })
 
-app.get('/convert', function (req, res) {
-  console.log("request to /convert was received");
-  res.send();
+// app.get('/convert', function (req, res) {
+//   console.log(req);
+
+//   res.send();
+// })
+
+//handles post request to enopoint /convert
+app.post('/convert', function (req, res) {
+
+  console.log("post request to /convert was received");
+
+  //convert data recevied back to JSON
+  let body = [];
+  req.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    var parsedBody = JSON.parse(body);
+
+    //call the function to convert the JSON to csv
+    convertMyJson(parsedBody, (data) => {
+      res.send(data);
+
+    });
+  });
 })
 
+
+var convertMyJson = function (obj, callback) {
+
+  //capture columnHeaders
+  var columnNames = [];
+    for (var key in obj) {
+      if (key !== "children") {
+        columnNames.push(key);
+      } 
+    }
+  var columnNamesJoinedbyComma = columnNames.join(",");
+
+  //convert each nodes key values except for children key to csv and store in an array
+  var childrenArray = [];
+
+  //helperfunction to loop through children and store the values except for children key
+  var childrenLookUp = function(obj){
+    var childArray = [];
+      for (var key in obj) {
+        if (key != "children") {
+          childArray.push(obj[key]);          
+        }
+      }
+      childrenArray.push(childArray.join(","))
+
+    if (obj.children) {
+      obj.children.forEach((child) => (childrenLookUp(child)))
+    } else {
+      return;
+    }
+  }
+
+  //call to recurse through the object using the helper function
+  childrenLookUp(obj);
+
+
+  var csvArray = [];
+
+  csvArray.push(columnNamesJoinedbyComma);
+  var combinedCsvArray = csvArray.concat(childrenArray);
+
+  var csvFormat = combinedCsvArray.join("<br>");
+
+  callback(csvFormat);
+}
+
 app.listen(3000)
+
 
 
